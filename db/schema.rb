a2da5bd3670d9c75dc9924e2e3bb30a893c3041a
windserenity <95578077+windserenity@users.noreply.github.com>
2022-08-23 20:41:54 -0700

    db_migrate- add columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_031051) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_033800) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
