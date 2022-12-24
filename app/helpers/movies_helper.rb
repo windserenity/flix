ed1a47affd4db21af2525757236585f14590c8ac
windserenity <95578077+windserenity@users.noreply.github.com>
2022-12-23 16:39:55 -0800

    use has_review method to display reviews

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -11,4 +11,19 @@ module MoviesHelper
     def year_of(movie)
        movie.released_on.year
     end
+
+    def average_stars(movie)
+        if movie.average_stars.zero?
+          content_tag(:strong, "No reviews")
+        else
+          pluralize(movie.average_stars, "star")
+        end
+    end
+
+    def display_reviews(movie)
+      if movie.has_review?
+          link_to pluralize(movie.reviews.size, "review"), movie_reviews_path(movie)
+
+     end   
+    end
 end
