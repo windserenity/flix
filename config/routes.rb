7866e00dad9250df84d7799b323dfca9cb628f4a
windserenity <95578077+windserenity@users.noreply.github.com>
2023-01-17 20:47:44 -0800

    user model scafold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+
   root "movies#index"
   
   resources :movies do
     resources :reviews  
   end
-
+  
+  resources :users
+  get "sign-up" => "users#new"
 end
