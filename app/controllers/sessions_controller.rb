4070c5b7c5bdd8edba3c09578d38f579b705616e
windserenity <95578077+windserenity@users.noreply.github.com>
2023-01-25 05:41:13 -0800

    implement user_signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,17 @@ class SessionsController < ApplicationController
 
 
     def create
-    
+        
+        user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+        
+         if user && user.authenticate(params[:password])
+           session[:user_id] = user.id
+           redirect_to user, notice: "Welcome back, #{user.name}!"
+        else
+            flash.now[:alert] = "Invalid email/password combination!"
+            render :new, status: :unprocessable_entity
+        end
+
     end
 
     def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ end
 def create
     @user = User.new(user_params)
     if @user.save 
+        session[:user_id] = @user.id
         redirect_to user_path(@user), notice: "Thanks for signing up!"
     else
       render :new, status: :unprocessable_entity
@@ -38,7 +39,7 @@ end
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to movies_url, status: :see_other,
+    redirect_to root_path, status: :see_other,
       alert: "Account successfully deleted!"
   end
 
