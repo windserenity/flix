9406582e62444a103c4254685136be50fec66e40
windserenity <95578077+windserenity@users.noreply.github.com>
2023-01-29 10:11:42 -0800

    run migration user admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_140042) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_180839) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_140042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
