7e9bda7bc78d1b39531ec7dd59bdb2c0f1f566c7
windserenity <95578077+windserenity@users.noreply.github.com>
2023-01-29 10:09:16 -0800

    save intendend url for redirect  in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ private
  
     def require_signin 
         unless  current_user 
+           session[:intended_url] = request.url
            redirect_to new_session_path, alert: "Please Sign in first!" 
         end
     end
