5df4152475abed976b16b435bf1045131b295fda
windserenity <95578077+windserenity@users.noreply.github.com>
2023-01-29 19:51:51 -0800

    remove user and add one_to_many with users.

---
+++ db/migrate/20230130034720_make_reviews_a_join_table.rb
@@ -0,0 +1,9 @@
+class MakeReviewsAJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :reviews, :name, :string
+    add_column :reviews, :user_id, :integer
+    Review.delete_all
+  end
+end
+
+  
